5bba458ae5a2aaebd8534e1124462359422db6c9
dearto20 <85853641+dearto20@users.noreply.github.com>
2024-06-28 21:29:48 +0900

    add support for text/html in email

--- app/src/main/java/com/example/llmwithrag/knowledge/apps/EmailAppManager.java
+++ app/src/main/java/com/example/llmwithrag/knowledge/apps/EmailAppManager.java
@@ -18,12 +18,16 @@ import com.example.llmwithrag.llm.EmbeddingManager;
 import com.sun.mail.imap.IMAPFolder;
 import com.sun.mail.imap.IMAPStore;
 
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 import java.util.Properties;
 import java.util.UUID;
 
+import javax.mail.BodyPart;
 import javax.mail.Folder;
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -31,6 +35,7 @@ import javax.mail.Session;
 import javax.mail.event.MessageCountAdapter;
 import javax.mail.event.MessageCountEvent;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMultipart;
 
 public class EmailAppManager implements IKnowledgeComponent {
     private static final String TAG = EmailAppManager.class.getSimpleName();
@@ -132,10 +137,18 @@ public class EmailAppManager implements IKnowledgeComponent {
                         String timeString = new SimpleDateFormat("HH:mm:ss", Locale.getDefault())
                                 .format(date);
                         String subject = message.getSubject();
-                        String body = message.getContent().toString();
+                        String body = getBody(message);
+
+                        Log.i(TAG, "address : " + address);
+                        Log.i(TAG, "sender : " + sender);
+                        Log.i(TAG, "subject : " + subject);
+                        Log.i(TAG, "body : " + body);
+                        Log.i(TAG, "date : " + dateString);
+                        Log.i(TAG, "time : " + timeString);
+
 
                         Entity emailEntity = new Entity(UUID.randomUUID().toString(),
-                                ENTITY_TYPE_EMAIL, body);
+                                ENTITY_TYPE_EMAIL, subject);
                         emailEntity.addAttribute("address", address);
                         emailEntity.addAttribute("sender", sender);
                         emailEntity.addAttribute("subject", subject);
@@ -193,5 +206,46 @@ public class EmailAppManager implements IKnowledgeComponent {
                 e2.printStackTrace();
             }
         }
+    }
+
+    private String getBody(Message message) {
+        try {
+            if (message.isMimeType("text/plain")) {
+                Log.i(TAG, "1");
+                return message.getContent().toString();
+            } else if (message.isMimeType("text/html")) {
+                Log.i(TAG, "2");
+                return Jsoup.parse((String) message.getContent()).text();
+            } else if (message.isMimeType("multipart/*")) {
+                Log.i(TAG, "3");
+                MimeMultipart mimeMultipart = (MimeMultipart) message.getContent();
+                return getTextFromMimeMultipart(mimeMultipart);
+            }
+        } catch (Throwable e) {
+            Log.e(TAG, e.toString());
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    private String getTextFromMimeMultipart(MimeMultipart mimeMultipart) throws MessagingException, IOException {
+        StringBuilder htmlText = new StringBuilder();
+        StringBuilder plainText = new StringBuilder();
+        int count = mimeMultipart.getCount();
+        for (int i = 0; i < count; i++) {
+            BodyPart bodyPart = mimeMultipart.getBodyPart(i);
+            if (bodyPart.isMimeType("text/plain")) {
+                plainText.append(bodyPart.getContent().toString());
+            } else if (bodyPart.isMimeType("text/html")) {
+                htmlText.append(Jsoup.parse((String) bodyPart.getContent()).text());
+            } else if (bodyPart.isMimeType("multipart/*")) {
+                if (bodyPart.isMimeType("text/plain")) {
+                    plainText.append(bodyPart.getContent().toString());
+                } else if (bodyPart.isMimeType("text/html")) {
+                    htmlText.append(Jsoup.parse((String) bodyPart.getContent()).text());
+                }
+            }
+        }
+        return htmlText.length() > 0 ? htmlText.toString() : plainText.toString();
     }
 }
